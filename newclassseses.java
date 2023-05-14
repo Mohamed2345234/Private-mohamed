7a0053a51dca02dab3eea0ddd15e7e6f64faaf09
Mohamed2345234 <127730693+Mohamed2345234@users.noreply.github.com>
2023-05-14 23:46:34 +0200

    Update newclassseses.java

--- newclassseses.java
+++ newclassseses.java
@@ -1,19 +1,30 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
 
-class OuderTest{
-    @Test
-    void testRedenGeaccepteerdTrue(){
-        Ouder moeder = new Ouder("Hanan" , "Mahamud", 47);
-        // Reden wordt geaccepteert
-        Assertions.assertDoesNotThrow(() -> moeder.accepteerOfWeigerRedenVanKind(true,true, true , true ));
-        Assertions.assertDoesNotThrow(() -> moeder.accepteerOfWeigerRedenVanKind(true,false, true, true ));
-        Assertions.assertDoesNotThrow(() -> moeder.accepteerOfWeigerRedenVanKind(true,true, false ,true ));
+class OuderPairwiseTesting {
 
-        //Reden wordt niet geaccepteert
-        Assertions.assertDoesNotThrow(() -> moeder.accepteerOfWeigerRedenVanKind(true,false, false, false));
-        Assertions.assertDoesNotThrow(() -> moeder.accepteerOfWeigerRedenVanKind(false,true, true, false ));
-        Assertions.assertDoesNotThrow(() -> moeder.accepteerOfWeigerRedenVanKind(false,false, true, false ));
-        Assertions.assertDoesNotThrow(() -> moeder.accepteerOfWeigerRedenVanKind(false,false, false , false ));
+    public static void main(String[] args) {
+        // Define all possible values for each input parameter
+        List<Boolean> isRedenRelevantValues = Arrays.asList(true, false);
+        List<Boolean> gaatOverSchoolValues = Arrays.asList(true, false);
+        List<Boolean> gaatOverBijbaanValues = Arrays.asList(true, false);
+        List<Boolean> redenGeaccepteerdValues = Arrays.asList(true, false);
+
+        // Perform pairwise testing by creating all possible pairs of input parameter values
+        for (Boolean isRedenRelevant : isRedenRelevantValues) {
+            for (Boolean gaatOverSchool : gaatOverSchoolValues) {
+                for (Boolean gaatOverBijbaan : gaatOverBijbaanValues) {
+                    for (Boolean redenGeaccepteerd : redenGeaccepteerdValues) {
+                        System.out.println("Test case:");
+                        System.out.println("isRedenRelevant = " + isRedenRelevant);
+                        System.out.println("gaatOverSchool = " + gaatOverSchool);
+                        System.out.println("gaatOverBijbaan = " + gaatOverBijbaan);
+                        System.out.println("redenGeaccepteerd = " + redenGeaccepteerd);
+                        System.out.println("-----------------------------------");
+                    }
+                }
+            }
+        }
     }
+
 }
